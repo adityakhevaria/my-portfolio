b41dce098f70013b310f060c8020d67de43e04bd
Aditya Khevaria <91670196+adityakhevaria@users.noreply.github.com>
2024-09-19 23:57:38 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               title="Ceasar-Cipher"
               description="Caesar Cipher is used to encode and decode the messages . We need to enter the the message in the text box and also enter a key value and then click on encrypt button. The message is encrypted to the user and can be decrypted with key"
               ghLink="https://github.com/adityakhevaria/Caecer-Cipher.git"
-              demoLink="caecer-cipherr.vercel.app"
+              demoLink="https://caecer-cipherr.vercel.app/"
             />
           </Col>
 
@@ -50,7 +50,7 @@ function Projects() {
               title="Todo-App"
               description="This sleek and intuitive application helps users efficiently manage their tasks and stay organized. With features like task creation, prioritization, and due dates. The app’s clean interface and responsive design ensure a smooth experience across various devices."
               ghLink="https://github.com/adityakhevaria/To-do-App"
-              demoLink="myown-todo-app.vercel.app"
+              demoLink="https://myown-todo-app.vercel.app"
             />
           </Col>
 
@@ -61,7 +61,7 @@ function Projects() {
               title="Blog-Page"
               description="This dynamic blog page provides insightful articles, expert opinions, and engaging content on a variety of topics. With an easy-to-navigate layout and clean design, readers can effortlessly explore posts, categories, and comments. The page ensures a visually appealing and user-friendly experience for both casual readers and dedicated followers"
               ghLink="https://github.com/adityakhevaria/Blog-page"
-              demoLink="blog-pagee.vercel.app"
+              demoLink="https://blog-pagee.vercel.app"
             />
           </Col>
 
@@ -72,7 +72,7 @@ function Projects() {
               title="Incubation-Center-Website"
               description="This comprehensive platform connects startups with the resources, mentorship, and funding they need to thrive. With a focus on fostering innovation, the website offers detailed information on programs, facilities, and success stories. Its user-friendly interface ensures seamless navigation for entrepreneurs, investors, and partners alike."
               ghLink="https://github.com/adityakhevaria/incubation-centre-django"
-              demoLink="incubation-center.vercel.app"
+              demoLink="https://incubation-center.vercel.app"
             />
           </Col>
 
@@ -83,7 +83,7 @@ function Projects() {
               title="Drum-Kit"
               description="This interactive and engaging app allows users to play a virtual drum kit with ease. Featuring a variety of drum sounds and customizable kits, it offers a realistic drumming experience for beginners and professionals alike. The app’s intuitive layout and responsive design make it accessible across devices for a fun and creative experience"
               ghLink="https://github.com/adityakhevaria/Drum-Kit"
-              demoLink="my--drum-kit.vercel.app"
+              demoLink="https://my--drum-kit.vercel.app"
             />
           </Col>
         </Row>
